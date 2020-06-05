d3cf3bf5fad54ed4e30932fb5974944daef15249
Scott Wang <ngwattcos@google.com>
2020-06-05 18:54:23 -0400

    change ViewEncapsulation to Emulated

--- my-angular-site/src/app/app.component.ts
+++ my-angular-site/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { YellowBackgroundComponent } from '../app/yellow-background/yellow-background.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  links = ['Learn Angular', 'Documentation', 'Unlearn React'];
   title = 'my-angular-site';
 }
--- my-angular-site/src/app/yellow-background/yellow-background.component.ts
+++ my-angular-site/src/app/yellow-background/yellow-background.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-yellow-background',
   templateUrl: './yellow-background.component.html',
-  styleUrls: ['./yellow-background.component.css']
+  styleUrls: ['./yellow-background.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class YellowBackgroundComponent implements OnInit {
 
